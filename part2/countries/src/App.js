31ba5a30e7805888d11258e2970c154b323724dd
subhakar-tjs <joseph.nn98@gmail.com>
2021-06-04 12:02:23 +0530

    2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,23 +2,56 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+  const [countries, setCountries] = useState([])
+  const [filtered, setFiltered] = useState('')
 
   useEffect(() => {
-    console.log('effect')
     axios
-      .get('http://localhost:3001/persons')
+      .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+        setCountries(response.data)
       })
   }, [])
-  
 
+  const handleChange = (event) => {
+    setFiltered(event.target.value)
+  }
+  const display = countries.filter(c => c.name.toString().toLowerCase().includes(filtered.toString().toLowerCase()) === true)
+
+    const handleClick = name => {
+    setFiltered(name)
+  }
+  let text;
+  if(filtered === "") {
+    text = <p>Search for country</p>
+  } else if (display.length > 10) {
+    text = <p>Too many matches</p>
+  } else if (display.length > 1) {      
+    text = display.map(d => 
+      <p key={d.alpha3Code}>
+        {d.name}
+        
+      </p>)
+  } else {
+    text = display.map(d => 
+      <div key={d.alpha3Code}>
+        <h2>{d.name}</h2>
+        <p>capital {d.capital}</p>
+        <p>population {d.population}</p>
+        <h2>languages</h2>
+        <ul>
+          {d.languages.map(l => <li key={l.name}>{l.name}</li>)}
+        </ul>
+        <img src={d.flag} height='200px' width='400px' alt='flag'/>
+      </div>
+      )
+    
+  }
 
   return (
-    <div >
-     
+    <div>
+    find countries  <input onChange={handleChange}/>
+     {text}
     </div>
   );
 }
