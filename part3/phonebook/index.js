a06adf17f84dc7a3234fc76ac6b850ef70000dd4
subhakar-tjs <71774625+subhakar-tjs@users.noreply.github.com>
2021-06-05 16:28:17 +0530

    updated  3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -51,9 +51,9 @@ let persons = [
     }
   })
 
-  /*app.delete('/api/persons/:id', (request, response) => {
+  app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(p => p.id !== id)
   
     response.status(204).end()
-  })*/
+  })
